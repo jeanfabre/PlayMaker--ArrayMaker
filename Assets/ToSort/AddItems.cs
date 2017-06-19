6b5d4d2a960d1db0fe740572f598400d5bf50f4b
Jean Fabre <contact@fabrejean.net>
2017-06-19 15:11:50 +0300

    safe commit

---
+++ Assets/ToSort/AddItems.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AddItems : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		Hashtable _tableInfo = GetComponent<PlayMakerHashTableProxy>().hashTable;
+		
+
+		//Update the tableInfo hash with new data
+		_tableInfo.Add("tableID", null);
+		_tableInfo.Add("numPlayers", 12);
+		_tableInfo.Add("numRuns", 34);
+		_tableInfo.Add("gameNum", 123);
+
+	}
+	
+
+}
